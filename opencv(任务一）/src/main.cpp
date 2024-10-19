6539f7faa97fb4e3d71fd4e1e2cb91e17809c6c0
“LIN-LOUIS” <linzhiyi060208.com>
2024-10-19 15:31:43 +0800

    opencv的第三次作业

--- opencv(任务一）/src/main.cpp
+++ opencv(任务一）/src/main.cpp
@@ -6,31 +6,48 @@ using namespace std;
 
 int main() {
     // 读取图像
-    Mat image = imread("../image/image1.jpg",IMREAD_GRAYSCALE);
+    Mat image = imread("../image/回形针.png");
     if (image.empty()) {
-        std::cerr << "Error: Could not open or find the image!" << std::endl;
+        cout << "无法读取图像" << endl;
         return -1;
     }
-
-    // 应用高斯模糊以减少噪声
-    Mat blurred_image;
-    GaussianBlur(image, blurred_image, Size(5, 5), 1.5);
-
-    // 应用 Canny 边缘检测
+    // 转换为灰度图像
+    Mat gray;
+    cvtColor(image, gray, COLOR_BGR2GRAY);
+    //二值化
     Mat edges;
-    Canny(blurred_image, edges, 50, 150);
+    threshold(gray, edges, 0, 255,  THRESH_OTSU);
+    // 执行 Canny 边缘检测
+    cv::Canny(edges, edges, 100, 200);
+    // 查找轮廓
+    std::vector<std::vector<cv::Point>> contours;
+    cv::findContours(edges, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
 
-    //改变图像大小
-    resize(edges, edges, Size(500, 500));
-    resize(image, image, Size(500, 500));
-
-    // 显示原图和边缘检测后的图像
-      ::imshow("Original Image", image);
-      ::imshow("Canny Edges", edges);
-
-    // 等待按键按下，关闭窗口
-      ::waitKey(0);
-      ::destroyAllWindows();
+    // 在原图上绘制轮廓的边界框
+    int i=0;
+    cv::Mat result = image.clone(); // 创建一个克隆图像以便绘制
+    for (const auto& contour : contours) {
+        // 计算轮廓的最小外接矩形
+        cv::RotatedRect minAreaRect = cv::minAreaRect(contour);
+        // 获取矩形的四个顶点
+        cv::Point2f rectPoints[4];
+        minAreaRect.points(rectPoints);
+        // 绘制最小外接矩形
+        for (int j = 0; j < 4; j++) {
+            cv::line(result, rectPoints[j], rectPoints[(j + 1) % 4], cv::Scalar(0, 255, 0), 2);
+        }
+        // 在矩形的左上角添加文本标记
+        i++;
+        string text=to_string(i);
+        cv::putText(result, text, rectPoints[0], cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 0,255), 3);
+    }
+    //保存结果
+    cv::imwrite("../image/回形针结果.png", result);
+    // 显示结果
+    cv::imshow("Original", image);
+    cv::imshow("Edges", edges);
+    cv::imshow("Bounding Boxes", result);
+    cv::waitKey(0);
 
     return 0;
 }
